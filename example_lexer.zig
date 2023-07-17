dbb61599fd9464fe7b06168224b96b9b99353148
MSB246 <msembulut@gmail.com>
2023-07-17 19:22:19 +0200

    Finished basic tools

--- example_lexer.zig
+++ example_lexer.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 const lexer = @import("lexer.zig");
+const cwd = std.fs.cwd();
+
+const MEMORY = 8192;
 
 fn is_star(byte: u8) bool {
     return byte == '*';
@@ -16,13 +19,33 @@ fn is_word(bytes: []const u8) usize {
 }
 
 const Foo = struct {
+    comptime whitespace: fn(u8) bool = std.ascii.isWhitespace,
     comptime plus: u8 = '+',
     comptime minus: u8 = '-',
     comptime star: fn(u8) bool = is_star,
     comptime slash: fn(u8) bool = is_slash,
+    comptime keyword: []const u8 = "key",
     comptime word: fn([]const u8) usize = is_word,
 };
 
-pub fn main() void {
-    lexer.tokenize(Foo, "2//+*+-+*+---//word");
+pub fn main() !void {
+    var buffer: [MEMORY]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+
+    const file = try cwd.openFile("input.nor", .{});
+    const input = try file.readToEndAlloc(allocator, MEMORY);
+    const tokens = try lexer.lex(Foo, allocator, input);
+
+    for (tokens) |token| {
+        switch (token) {
+            .whitespace => std.debug.print("Token.whitespace\n", .{}),
+            .plus => std.debug.print("Token.plus\n", .{}),
+            .minus => std.debug.print("Token.minus\n", .{}),
+            .star => std.debug.print("Token.star\n", .{}),
+            .slash => std.debug.print("Token.slash\n", .{}),
+            .keyword => std.debug.print("Token.keyword\n", .{}),
+            .word => |word| std.debug.print("Token.word: \"{s}\"\n", .{ word }),
+        }
+    }
 }
--- lexer.zig
+++ lexer.zig
@@ -1,33 +1,112 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 
-pub fn tokenize(comptime T: type, input: []const u8) void {
+fn isPrefix(a: []const u8, b: []const u8) bool {
+    if (a.len < b.len) return false;
+    for (0..b.len) |i| if (a[i] != b[i]) return false;
+    return true;
+}
+
+fn getMinSize(comptime len: usize) type {
+    if (len == 0) return u0;
+    return switch (std.math.log2_int_ceil(usize, len - 1)) {
+        1...8 => u8,
+        9...16 => u16,
+        17...32 => u32,
+        33...64 => u64,
+        else => usize,
+    };
+}
+
+fn createEnum(comptime T: type) type {
+    const tFields = std.meta.fields(T);
+    var fields: [tFields.len]std.builtin.Type.EnumField = undefined;
+
+    for (0..tFields.len) |i| {
+        fields[i] = .{
+            .name = tFields[i].name,
+            .value = i,
+        };
+    }
+
+    return @Type(.{
+        .Enum = .{
+            .tag_type = getMinSize(tFields.len),
+            .fields = &fields,
+            .decls = &[0]std.builtin.Type.Declaration{},
+            .is_exhaustive = true,
+        },
+    });
+}
+
+pub fn createToken(comptime T: type) type {
+    const tFields = std.meta.fields(T);
+    var fields: [tFields.len]std.builtin.Type.UnionField = undefined;
+
+    for (0..tFields.len) |i| {
+        const fieldType = switch (tFields[i].type) {
+            fn ([]const u8) usize => []const u8,
+            fn (u8) bool, []const u8, u8 => void,
+            else => unreachable,
+        };
+
+        fields[i] = .{
+            .name = tFields[i].name,
+            .type = fieldType,
+            .alignment = 0,
+        };
+    }
+
+    return @Type(.{
+        .Union = .{
+            .layout = .Auto,
+            .tag_type = createEnum(T),
+            .fields = &fields,
+            .decls = &[0]std.builtin.Type.Declaration{},
+        },
+    });
+}
+
+pub fn lex(comptime T: type, allocator: Allocator, input: []const u8) ![]createToken(T) {
+    const Token = createToken(T);
+    var tokens = try allocator.alloc(Token, 0);
     var i: usize = 0;
 
-    while (i < input.len) : (i += 1) {
+    chars: while (i < input.len) {
         inline for (std.meta.fields(T)) |field| {
-            //const aligned_value = @alignCast(field.alignment, default_value);
-            //const value = @ptrCast(*const field.type, aligned_value);
             const value = if (field.default_value) |default_value|
                 @ptrCast(*const field.type, @alignCast(field.alignment, default_value)).*
             else
                 unreachable;
 
-            switch (field.type) {
-                fn ([]const u8) usize => {
-                    const len = value(input[i..]);
-                    if (len != 0) {
-                        std.debug.print("{s} = \'{d}\'\n", .{ field.name, len });
-                        i += len - 1;
-                    }
-                },
-                fn (u8) bool => if (value(input[i])) {
-                    std.debug.print("{s} = \'{c}\'\n", .{ field.name, input[i] });
-                },
-                u8 => if (input[i] == value) {
-                    std.debug.print("{s} = \'{c}\'\n", .{ field.name, input[i] });
-                },
-                else => unreachable,
+            var matched: bool = true;
+            var len: usize = undefined;
+            var token: Token = undefined;
+            if (field.type == fn ([]const u8) usize and value(input[i..]) != 0) {
+                len = value(input[i..]);
+                token = @unionInit(Token, field.name, input[i .. i + len]);
+            } else if (field.type == fn (u8) bool and value(input[i])) {
+                len = 1;
+                token = @unionInit(Token, field.name, undefined);
+            } else if (field.type == []const u8 and isPrefix(input[i..], value)) {
+                len = value.len;
+                token = @unionInit(Token, field.name, undefined);
+            } else if (field.type == u8 and input[i] == value) {
+                len = 1;
+                token = @unionInit(Token, field.name, undefined);
+            } else matched = false;
+
+            if (matched) {
+                tokens = try allocator.realloc(tokens, tokens.len + 1);
+                tokens[tokens.len - 1] = token;
+
+                i += len;
+                continue :chars;
             }
         }
+
+        unreachable;
     }
+
+    return tokens;
 }
